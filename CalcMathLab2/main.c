04bd5dcdcbd96efecd591524e57112c4bfe768f5
Alex Nekhaev <alexnekhaev@gmail.com>
2018-10-30 11:00:20 +0300

    Initial Commit

---
+++ CalcMathLab2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CalcMathLab2
+//
+//  Created by Alexander Nekhaev on 30.10.2018.
+//  Copyright © 2018 Alexander Nekhaev. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
